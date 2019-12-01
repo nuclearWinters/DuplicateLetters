ac521ce977351e6fa2c333e6054ab36f59f96d3a
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2019-12-01 18:28:51 -0500

    first

---
+++ src/main/java/com/mycompany/duplicateletters/DuplicateLetters.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.duplicateletters;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Armando
+ */
+ 
+public class DuplicateLetters 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Introduce un texto: ");
+        Scanner codigo = new Scanner(System.in);
+        String blogName = codigo.nextLine();
+        blogName = blogName.toLowerCase();
+        char[] chars = blogName.toCharArray();
+         
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : chars) 
+        {
+            if(map.containsKey(c)) {
+                int counter = map.get(c);
+                map.put(c, ++counter);
+            } else {
+                map.put(c, 1);
+            }
+        }
+         
+        System.out.println("Duplicate characters excluding white space :");
+         
+        //Print duplicate characters excluding white space
+        for(char c : map.keySet()) {
+            if(map.get(c) > 1 && Character.isLetter(c)) {
+                System.out.println(c);
+            }
+        }
+    }
+}
